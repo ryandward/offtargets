b020e9ebb2471a5a43d6576e13be1f20dfd78e89
Ryan Ward <ryandward@gmail.com>
2023-03-30 17:44:03 -0500

    add: dissectgb turns gb into fasta and bed

---
+++ dissectgb.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+import sys
+import argparse
+import gzip
+from Bio import SeqIO
+from Bio.SeqRecord import SeqRecord
+from Bio.SeqFeature import FeatureLocation
+from pathlib import Path
+
+def read_genbank_file(file_path):
+    if file_path.suffix == '.gz':
+        with gzip.open(file_path, 'rt') as handle:
+            records = list(SeqIO.parse(handle, 'genbank'))
+    else:
+        with file_path.open('r') as handle:
+            records = list(SeqIO.parse(handle, 'genbank'))
+    return records
+
+def write_fasta(records, output_fasta):
+    fasta_records = []
+    for record in records:
+        fasta_records.append(SeqRecord(record.seq, id=record.id, description=record.description))
+    SeqIO.write(fasta_records, output_fasta, 'fasta')
+
+def write_bed(records, output_bed):
+    with open(output_bed, 'w') as bed_file:
+        for record in records:
+            for feature in record.features:
+                locus_tag = feature.qualifiers.get('locus_tag', [''])[0].strip()
+                gene_name = feature.qualifiers.get('gene', [''])[0].strip()
+                strand = '+' if feature.location.strand > 0 else '-'
+                feature_type = feature.type.strip()
+                product = feature.qualifiers.get('product', [''])[0].strip()
+
+                bed_line = [record.id.strip(), str(feature.location.start), str(feature.location.end),
+                            locus_tag, gene_name, strand, feature_type, product]
+                bed_file.write('\t'.join(bed_line) + '\n')
+
+def main():
+    parser = argparse.ArgumentParser(description='Convert GenBank files to FASTA and BED formats')
+    parser.add_argument('input_files', metavar='input_files', nargs='+', help='Input GenBank (gb or gb.gz) files')
+    parser.add_argument('--name', type=str, required=True, help='Output file prefix')
+    args = parser.parse_args()
+
+    all_records = []
+    for input_file in args.input_files:
+        all_records.extend(read_genbank_file(Path(input_file)))
+
+    output_fasta = f"{args.name}.fasta"
+    output_bed = f"{args.name}.bed"
+
+    write_fasta(all_records, output_fasta)
+    write_bed(all_records, output_bed)
+
+if __name__ == '__main__':
+    main()
+
