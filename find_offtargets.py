c22fc5541a8ce458f9f792c0c58a230b745c7d6f
Ryan Ward <ryandward@gmail.com>
2023-03-30 16:02:09 -0500

    fix: sped by using regex

--- find_offtargets.py
+++ find_offtargets.py
@@ -1,15 +1,16 @@
 import sys
 import gzip
+import regex
 from Bio import SeqIO
-from Bio import pairwise2
 from Bio.Seq import Seq
 
-# Load the list of potential off-targeting elements from a text file
+# Load the list of potential off-targeting elements from a FASTA file
 with open(sys.argv[1], 'r') as f:
-    off_target_elements = [line.strip() for line in f]
+    off_target_elements = [(record.description, str(record.seq)) for record in SeqIO.parse(f, "fasta")]
 
 # Set a score cutoff (adjust as needed)
 score_cutoff = 16  # only report alignments with a score of 16 or higher
+max_mismatches = len(off_target_elements[0][1]) - score_cutoff
 
 # Check if the position is within a gene
 def within_gene(start, features):
@@ -32,7 +33,7 @@ def get_differences(query, target):
     return ', '.join(differences)
 
 # Print header for TSV output
-header = ["query_seq", "off_target_seq", "record_id", "position", "score", "in_gene", "gene_name", "locus_tag", "differences", "orientation"]
+header = ["query", "q_seq", "q_len", "off_target_seq", "chromosome", "position", "score", "in_gene", "gene_name", "locus_tag", "differences", "orientation"]
 print('\t'.join(header))
 
 # Iterate over the input GenBank files
@@ -45,21 +46,13 @@ for genome_file in sys.argv[2:]:
         for record in genome_records:
             seq = str(record.seq)
             seq_rc = str(record.seq.reverse_complement())
-            for i in range(len(seq) - len(off_target_elements[0]) + 1):
-                subseq_fwd = seq[i:i+len(off_target_elements[0])]
-                subseq_rev = seq_rc[i:i+len(off_target_elements[0])]
-                for off_target_element in off_target_elements:
-                    for subseq, orientation in [(subseq_fwd, "forward"), (subseq_rev, "reverse")]:
-                        score = sum(1 for a, b in zip(off_target_element, subseq) if a == b)
-
-                        if score >= score_cutoff:
-                            if orientation == "reverse":
-                                position = len(seq) - (i + len(off_target_element))
-                            else:
-                                position = i
-
-                            in_gene, gene_name, locus_tag = within_gene(position, record.features)
-                            differences = get_differences(off_target_element, subseq)
-                            output = [off_target_element, subseq, record.id, str(position), str(score), str(in_gene), gene_name, locus_tag, differences, orientation]
-                            print('\t'.join(output))
-
+            for query_description, off_target_element in off_target_elements:
+                pattern = f"({off_target_element}){{s<={max_mismatches}}}"
+                for orientation, sequence in [("forward", seq), ("reverse", seq_rc)]:
+                    for match in regex.finditer(pattern, sequence):
+                        score = len(off_target_element) - match.fuzzy_counts[0]
+                        position = match.start()
+                        in_gene, gene_name, locus_tag = within_gene(position, record.features)
+                        differences = get_differences(off_target_element, match.group())
+                        output = [query_description, off_target_element, str(len(off_target_element)), match.group(), record.id, str(position), str(score), str(in_gene), gene_name, locus_tag, differences, orientation]
+                        print('\t'.join(output))
